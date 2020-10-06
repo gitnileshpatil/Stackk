626a92788e283d3b5bcd05235b238fc5b9b7c45e
Nilesh <patil_nilesh55@ymail.com>
2020-10-06 20:28:24 +0530

    Add project files.

---
+++ Stackk/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  Output:
+                Current Stack: [ 1  2  3 ]
+                Object Removed: 3
+                Current Stack: [ 1  2 ]
+                Object Removed: 2
+                Current Stack: [ 1 ]
+                Object Removed: 1
+                Current Stack: []
+             */
+            StackModel stack = new StackModel();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            stack.DisplayStack();
+
+            Console.WriteLine("Object Removed: " + stack.Pop());
+            stack.DisplayStack();
+            Console.WriteLine("Object Removed: " + stack.Pop());
+            stack.DisplayStack();
+            Console.WriteLine("Object Removed: " + stack.Pop());
+            stack.DisplayStack();
+
+            Console.WriteLine();
+
+            stack.Clear();
+        }
+    }
+}
---
+++ Stackk/StackModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack
+{
+    public class StackModel             //Added a new .cs file as suggested
+    {
+        private int _currentIndex = -1;   //Assigned access modifiers to specify proper scope(acccess) + Changed the names of the private variables as per the naming standards
+                                          //private object _obj;              Got rid of the '_obj' variables as it was unnecessary here
+        private List<object> _stackList = new List<object>();        //Changed the casing.
+
+        public void Push(object obj)
+
+        {
+            if (obj != null)
+            {
+                _currentIndex++;
+                _stackList.Add(obj);
+            }
+            else
+            {
+                Console.WriteLine("Object is null");
+            }
+        }
+
+        public object Pop()
+        {
+            if (_currentIndex == -1)     //Changed if statement so that we check if the list is empty in the first place...before assigning anything
+            {
+                Console.WriteLine("Stack is Empty, nothing to pop.");
+                return null;
+            }
+            else
+            {
+                object obj = _stackList[_currentIndex];
+                _stackList.RemoveAt(_currentIndex);     //Tried using 'RemoveAt' instead of 'Remove'
+                _currentIndex--;
+                return obj;
+            }
+        }
+
+        public void Clear()
+        {
+            _stackList.Clear();
+        }
+
+        public void DisplayStack()      //Implemented DisplayStack() using a 'for each' loop instead of a 'for' loop
+        {
+            Console.Write("Current Stack: [");
+            foreach (object obj in _stackList)
+            {
+                Console.Write($" {obj} ");
+            }
+            Console.Write("]");
+            Console.WriteLine();
+        }
+    }
+}
